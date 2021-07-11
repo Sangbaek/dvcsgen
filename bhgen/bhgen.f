893dacc11bf5292de4411b614684169a8ae8ff29
Sangbaek <sangbaek@mit.edu>
2021-07-11 18:11:58 -0400

    works up to here, but egamma slightly different from before

--- bhgen/bhgen.f
+++ bhgen/bhgen.f
@@ -1,5 +1,6 @@
     	program main
       implicit real*8(a-h,k-m,o-z)
+#include "dvcs.inc"
 	    real*8 ebeam,s,q2,x,t,phi,egamma,thetag,phig,stot,delta
         integer*4 ipol,ikeygene,ichnnel,iddd
 	integer*4 iappr,hel
@@ -16,12 +17,16 @@
         data art/-4.045d0,-2.774d0,-1.504d0,-0.234d0/     
         data ardelta/1d-6,1d-5,1d-4,1d-3,1d-2,1d-1/
 
-	     pi=atan(1d0)*4d0
 		 
          ebeam=10.604
 
          ipol=0 !target polarization switch:0-unpolarized, 1-long, 2-trans along x-axis, 3-trans along y-axis
          iappr=2   !1-BH only, 2-BMK 
+         Ivar = 3
+         Ich = -1
+         IGPD = 3
+         Ipn = 1
+         Ed = 10.604
 
        if(iappr.eq.2)then
          hel=1     !             hel=1(0) to (not) include polarized part   
@@ -229,6 +234,7 @@ c		 do iddd=1,ndelta
           i=ikeyfo
 
 	  nev=2**i
+      print *, nev
 	  sum1=0d0
       sum2=0d0
 	  sum1delta=0d0
@@ -470,7 +476,7 @@ c           write(71,*)ichnnel,Egamma,thetag
           endif
 
 		  endif
-      print *, ichnnel, thetag, egamma
+      print *, ichnnel, thetag, egamma, probn, probs, probp
 		  
 
 
@@ -648,6 +654,7 @@ c      write(*,'(i4,6f8.4,i7)')ichnnel,egamma,missingmass2,complanarity,complana
             if(sphi.ge.0d0)phiphi=acos(min(1d0,max(-1d0,cphi)))
             if(sphi.lt.0d0)phiphi=2.*pi-acos(min(1d0,max(-1d0,cphi)))
             call bmkxsec(3, 3, 1, -1, 1,hel, s/2./mp, x, Q2, t,  pi-phiphi,siborn,eta,ipol,3)
+c            call bmkxsec2(x, Q2, t, pi-phiphi,pi-phiphi,siborn)
             return   
           endif
         
@@ -790,7 +797,8 @@ c
       Phi_gb=pi - Phi_g
 c      Phi_s=Phi_e
 c      call bhdvcs(Ivar,IGPD,Ipn,E,  xb,  Q2,  del2,  Phi_s,Phi_gb) 
-      call bhdvcs(Ivar,IGPD,Ipn,E,  xb,  Q2,  del2, Phi_gb) 
+c      call bhdvcs(Ivar,IGPD,Ipn,E,  xb,  Q2,  del2, Phi_gb) 
+      call bhdvcs(xb,  Q2,  del2, Phi_gb) 
 c
 c      dsBH =hc0BH +hc1BH*cos(Phi_gb)+hc2BH*cos(2D0*Phi_gb)
 c      dsBHlp =hel*help*(hc0BHlp +hc1BHlp*cos(Phi_gb))
@@ -856,8 +864,151 @@ c      print *,'out of limits ',xb,xmin1,xmax1,del2,del2min,del2max,istat
       return
       end
 c
+
+C        Ich  -  positron(electron) = +1(-1)
+C int   hel  -  lepton beam polarization
+C int   help -  target polarization
+C double  dsigma   d^5\sig / dx dQ^2 d|\Delta^2| d\phi_e d\phi_\gamma
+C                  (pb GeV^{-4})
+c
+      subroutine bmkxsec2(xb, Q2, del2, Phi_e, Phi_g,dsigma)
+      implicit none
+#include "dvcsmom.inc"
+#include "dvcs.inc"
+#include "ntupgdvcs.inc"
+      double precision E, xb, Q2, del2,Phi_e,Phi_g,Phi_s,Phi_gb,dsigma
+      double precision nu,W2,W,qmod,E1cm,P1cm,E2cm,P2cm,del2max,del2min
+      double precision yb,ymax,ycol
+      double precision  xmin1,xmax1
+      double precision dsIunp,dsBHlp, dsIlp,dsBHtp,dsItp
+      real corraul,rP1,rP2
+C       Ivar = 3
+C       Ich = -1
+C       IGPD = 3
+C       Ipn = 1
+c
+c
+      istatus=0                    ! kinematic range OK
+      Ed=cl_be
+      E=Ed
+      xmin1 = Q2/(2D0*Mp*E)
+      xmax1 = 1D0
+      nu  = Q2/(2D0*Mp*xb)
+      W2  = Mp**2 + 2D0*Mp*nu - Q2
+      W   = sqrt(W2)
+      qmod = sqrt(nu**2 + Q2)
+c
+      E1cm = Mp*(Mp + nu)/W
+      P1cm = Mp*qmod/W
+      E2cm = (W2 + Mp**2)/(2D0*W)
+      P2cm = (W2 - Mp**2)/(2D0*W)
+      del2max = 2D0*(Mp**2 - E1cm*E2cm - P1cm*P2cm)
+      del2min = 2D0*(Mp**2 - E1cm*E2cm + P1cm*P2cm)
+c
+      if( xb.le.xmin1 .or. xb.gt.xmax1 ) istatus=1           !    x  out of range
+      if( del2.ge.del2min .or. del2.le.del2max ) istatus=2   ! delta out of range
+      yb=nu/Ed
+      if(yb.gt.cl_ymax) istatus=3                               ! y<ymax
+      call dvcsycol(del2,xb,Q2,ycol)
+
+      Phi_gb=pi - Phi_g
+      Phi_s=Phi_e
+      call bhdvcs(xb,  Q2,  del2,  Phi_s,Phi_gb,rP1,rP2) 
+c      call bhdvcs(3,3,Ipn,E,  xb,  Q2,  del2, Phi_gb) 
+c
+      if(abs(rP1).le.cl_ycol ) istatus=4                   ! y-too big
+c      if((ycol-yb).le.cl_ycol ) istatus=4                   ! y-too big
+
+      if (istatus.eq.0) then
+c
+      dsBH =hc0BH +hc1BH*cos(Phi_gb)+hc2BH*cos(2D0*Phi_gb)
+      dsDVCS=hc0dvcs+hc1dvcs*cos(Phi_gb)+hs1dvcs*sin(Phi_gb)+hs2dvcs*sin(2*Phi_gb)
+      dsIunp=hc0Iunp +heli*hs1Iunp*sin(Phi_gb)+hc1Iunp*cos(Phi_gb) 
+     6     +heli*hs2Iunp*sin(2*Phi_gb)+hc2Iunp*cos(2*Phi_gb)
+c      
+c
+c      scale the sin\phi moment
+       dsIunp=dsIunp*cl_scale
+c
+       if(cl_ktcor) then
+         dsIunp=dsIunp*corraul(del2,Q2)     ! correct~20% of A_LU at small t
+       endif
+c
+       dsigma=dsBH
+       if(cl_bh.gt.1)  dsigma=dsigma+dsDVCS-Ich*dsIunp 
+
+c
+c      L-POL
+c
+       if(cl_pol.eq.1) then 
+         dsBHlp =heli*helpi*(hc0BHlp +hc1BHlp*cos(Phi_gb))
+         dsIlp=heli*helpi*hc0Ilp+heli*helpi*hc1Ilp*cos(Phi_gb)
+     +         +helpi*hs1Ilp*sin(Phi_gb)
+         dsigma=dsigma+dsBHlp -Ich*dsIlp
+       endif
+c
+c      T-POL
+c 
+       if(cl_pol.eq.2) then 
+         dsBHtp =heli*helpi*cos(Phi_s)*(hc0BHtpcos +hc1BHtpcos*cos(Phi_gb)) 
+     6       +heli*hs1BHtpsin*sin(Phi_s)*sin(Phi_gb)
+         dsItp=heli*hc0Itpcos*cos(Phi_s)+hc0Itpsin*sin(Phi_s)
+     6     +hc1Itpsin*cos(Phi_gb)*sin(Phi_s)+heli*hc1Itpcos*cos(Phi_gb)*cos(Phi_s)
+     6     +heli*hs1Itpsin*sin(Phi_gb)*sin(Phi_s)+hs1Itpcos*sin(Phi_gb)*cos(Phi_s)
+         dsigma=dsigma+dsBHtp -Ich*dsItp
+       endif
+c
+c
+c
+      else
+        dsigma=0
+c       print *,'out of limits ',xb,xmin1,xmax1,del2,del2min,del2max,istat      
+      endif
+       gwbh=dsBH
+       gwdvcs=dsDVCS
+       gvint=dsIunp
+c       gsin=hs1Iunp/(hc0BH+hc0dvcs+hc0Iunp)
+       gsin=hs1Iunp/hc0BH
+       gsin2=dsigma
+        gproh=helpi
+        ghp1=hcp1
+        ghp2=hcp2
+        ghccb=hccb
+        ghcci=hcci
+        gh0bh=hc0BH
+        gh1bh=hc1BH
+        gh2bh=hc2BH
+        gh0dvcs=hc0dvcs
+        gh1dvcs=hc1dvcs
+        ghs1dvcs=hs1dvcs
+        gh0iunp=hc0Iunp
+        gh1iunp=hc1Iunp
+        ghs1iunp=hs1Iunp
+        ghs2iunp=hs2Iunp
+       if(cl_pol.eq.1) then
+        gh0bhlp=hc0bhlp
+        gh1bhlp=hc1bhlp
+        ghs1ilp=hs1ilp
+        gh1ilp=hc1ilp
+        gh0ilp=hc0ilp
+       else if(cl_pol.eq.2) then
+        gh0bhtpcos = hc0BHtpcos
+        gh1bhtpcos = hc1BHtpcos
+        ghs1bhtpsin= hs1BHtpsin
+        ghs1itpcos = hs1Itpcos
+        ghs1itpsin = hs1Itpsin
+        gh1itpcos  = hc1Itpcos
+        gh1itpsin  = hc1Itpsin
+        gh0itpcos  = hc0Itpcos
+        gh0itpsin  = hc0Itpsin
+      endif
+c      print *,'bhdvcs rP1= ',rP1,rP2,dsigma,cl_ycol,ycol-yb,xb,Q2,del2
+      return
+      end
+c
+
 c      subroutine bhdvcs(Ivar,IGPD,Ipn,Ed, xbd, Q2d, del2d, phield, phigd)
-      subroutine bhdvcs(Ivar,IGPD,Ipn,Ed, xbd, Q2d, del2d,  phigd)
+      subroutine bhdvcs(xbd, Q2d, del2d,  phigd)
 c
 C BH+DVCS+INT by V. Korotkov
 C Tpol and Lpol added by  H.Avakian
@@ -879,12 +1030,11 @@ C                            BH, DVCS and interf. terms
 *
       implicit double precision (A-H,O-Z)
        include "dvcsmom.inc"
+       include "dvcs.inc"
 
       parameter ( alpha = 1D0/137.036D0, hc2  = 0.38938D0)
       parameter ( coeff = 1D+9*hc2*alpha**3 )
 c
-      double precision Mp, mele, pi
-      common/myconst/ Mp, mele, pi
 *
       double precision nu, k1pl, k2pl, Kfac, Jfac
 c      common/todvcs/ xb, yb, Q2, del2, del2min,phip,phipel,P1,P2,Kfac,ds
@@ -1020,7 +1170,7 @@ c
         endif
 c
         if( Ivar.eq.2 .or. Ivar.eq.3 ) then       
-         call dvcsfun( IGPD, Ipn)
+         call dvcsfun()
         endif
 c
 
@@ -1032,40 +1182,28 @@ c        stop
       return
       end
 
-      subroutine dvcsfun( IGPD, Ipn)
+      subroutine dvcsfun()
       implicit double precision (A-H,O-Z)
-       include "dvcsmom.inc"
-      double precision Kfac,Intfac
-c      common/todvcs/ x, y,Q2,del2,del2min,phip,phipel,P1,P2,Kfac,ds
-      common/todvcs/ x, y,Q2,del2,del2min,phip,P1,P2,Kfac,ds
+#include "dvcs.inc"
+#include "dvcsmom.inc"
+      double precision Kfac,Jfac,Intfac
+      common/todvcs/ x, y,Q2,del2,del2min,phip,phipel,P1,P2,Kfac,Jfac,ds
       common/formfac/ F1pn(2), F2pn(2)
-      double precision Mp, mele, pi
-      common/myconst/ Mp, mele, pi
-c
-      real zskew,zF1,zF2,zHp,zEp,zHtp,zEtp,zCIim
-      common /zgpds/zskew,zF1,zF2,zHp,zEp,zHtp,zEtp,zCIim
 c
       skew = x/(2D0 - x)
-      call amptab( IGPD, Ipn, skew, del2, 
+      if(IGPD.lt.100) then
+      call amptab(skew, del2, 
      &             H1_RE, H1_IM, H1T_RE, H1T_IM,
      &             E1_RE, E1_IM, E1T_RE, E1T_IM )
+      else
+      call amptabgag(skew, del2, 
+     &             H1_RE, H1_IM, H1T_RE, H1T_IM,
+     &             E1_RE, E1_IM, E1T_RE, E1T_IM )
+      endif
 *
-c
-c
 C         proton/neutron
       F1 = F1pn(Ipn)
       F2 = F2pn(Ipn)
-c
-c
-c      print *,skew,F1,F2,del2
-      zskew=skew
-      zF1=F1
-      zF2=F2
-      zHp=H1_IM
-      zEp=E1_IM
-      zHtp=H1T_IM
-      zEtp=E1T_IM
-c
 *
       deldel    = 1D0 - del2min/del2
       deldel_sq = sqrt(deldel)
@@ -1074,8 +1212,8 @@ c
       delm2_sq  = sqrt(del2m2)
       cy2   = 2D0 - 2D0*y + y**2
       Intfac=ds/(x*y**3*P1*P2*(-del2))
-c
-c
+      hcci=Intfac
+*
 C  DVCS
 C     
       a1 = H1_RE**2 + H1_IM**2 + H1T_RE**2 + H1T_IM**2
@@ -1092,18 +1230,13 @@ C
       DVCSfac=ds/(y**2*Q2)
       hc0dvcs=c0_DVCS*DVCSfac
       hc1dvcs=Kfac*c1_DVCS*DVCSfac
+      ac0dvcs=c0_DVCS
+      ac1dvcs=Kfac*c1_DVCS
 C
 C  INTERF
 C
-c
-c
       C_I_re = F1*H1_RE + x/(2D0-x)*(F1+F2)*H1T_RE - del2m4*F2*E1_RE
-      C_I_im1 = F1*H1_IM
-      C_I_im2 = x/(2D0-x)*(F1+F2)*H1T_IM
-      C_I_im3  = -del2m4*F2*E1_IM
-      
-      C_I_im = C_I_im1 + C_I_im2 + C_I_im3
-      zCIim=-C_I_im/F1 
+      C_I_im = F1*H1_IM + x/(2D0-x)*(F1+F2)*H1T_IM - del2m4*F2*E1_IM
       RE2    = x/(2D0-x)*(H1_RE + E1_RE) + H1T_RE
       C_I_re_eff = -x*C_I_re
       C_I_im_eff = -x*C_I_im
@@ -1116,13 +1249,18 @@ c
       c2_I = -16D0*((2D0 - y)/(2D0 - x))*C_I_re_eff
       s2_I =  16D0*(y/(2D0 - x))*C_I_im_eff
 c
+c
+C     moments
+c
       hs2Iunp=Kfac*Kfac*s2_I*Intfac
       hs1Iunp=Kfac*s1_I*Intfac
       hc2Iunp=Kfac*Kfac*c2_I*Intfac
       hc1Iunp=Kfac*c1_I*Intfac
       hc0Iunp=del2/Q2*c0_I*Intfac
 c
-c      print *, 'vvv ',hs1Iunp/hc0BH,C_I_im2/C_I_im1,C_I_im3/C_I_im1
+      ac0int=del2/Q2*c0_I
+      ac1int=Kfac*c1_I
+      as1int=Kfac*s1_I
 c
 c      print *,'mysl-dvcs',hs1Iunp,hc1Iunp,hc0Iunp
 C
@@ -1213,9 +1351,62 @@ c
       return
       end
 
-      subroutine amptab( IGPD, Ipn, skew, del2, 
+      subroutine amptabgag(skew, del2, 
      &                   H1_RE, H1_IM, H1T_RE, H1T_IM,
      &                   E1_RE, E1_IM, E1T_RE, E1T_IM )
+#include "dvcs.inc"
+c
+      REAL xi
+      REAL mt
+      REAL cff_re
+      REAL cff_im
+      REAL v_re,v_im
+c
+      double precision skew, del2, H1_RE, H1_IM, H1T_RE, H1T_IM,
+     &                             E1_RE, E1_IM, E1T_RE, E1T_IM 
+      double precision F1pn(2), F2pn(2)
+      common/formfac/ F1pn, F2pn
+      real mpi/0.1396/
+      data init/1/
+c
+      SAVE
+c
+      if( init .eq. 1 ) then
+        init = 0
+        call GPD_Init(IGPD-100)
+      endif
+*
+      F1u = 2D0*F1pn(1) + F1pn(2)
+      F1d = 2D0*F1pn(2) + F1pn(1)
+      F2u = 2D0*F2pn(1) + F2pn(2)
+      F2d = 2D0*F2pn(2) + F2pn(1)
+c
+      xi=skew
+      mt=-del2
+      v_re = GPD_GET_CFF(xi,mt,0)
+      v_im = GPD_GET_CFF(xi,mt,3)
+      H1_RE=-v_re
+      H1_IM=-v_im
+      v_re = GPD_GET_CFF(xi,mt,1)
+      v_im = GPD_GET_CFF(xi,mt,4)
+      H1T_RE=-v_re
+      H1T_IM=-v_im
+      v_re = GPD_GET_CFF(xi,mt,2)
+      v_im = GPD_GET_CFF(xi,mt,5)
+      E1_RE=-v_re
+      E1_IM=-v_im
+      E1T_IM=0
+      E1T_RE=0
+c
+      return
+      end
+c
+c
+c
+      subroutine amptab(skew, del2, 
+     &                   H1_RE, H1_IM, H1T_RE, H1T_IM,
+     &                   E1_RE, E1_IM, E1T_RE, E1T_IM )
+#include "dvcs.inc"
       double precision skew, del2, H1_RE, H1_IM, H1T_RE, H1T_IM,
      &                             E1_RE, E1_IM, E1T_RE, E1T_IM 
       double precision F1pn(2), F2pn(2)
@@ -1233,10 +1424,10 @@ c
       common/retbl1/ vh1urenf(51,21,2), vh1drenf(51,21,2) 
       common/imtbl1/ vh1uimnf(51,21,2), vh1dimnf(51,21,2)
 *
-      double precision Mp, mele, pi
-      common/myconst/ Mp, mele, pi
       real mpi/0.1396/
       data init/1/
+      real  h1uimag,h1dimag,h1tuimag,h1tdimag
+      common /h1imag/h1uimag,h1dimag,h1tuimag,h1tdimag
 *
       SAVE
 *
@@ -1329,12 +1520,18 @@ c
 *
         if( Ipn .eq. 1 ) then
           if( IGPD .le. 5 ) then
+      h1uimag=(F1u/2D0)*h1uim
+      h1dimag=(F1d/2D0)*h1dim
       H1_RE = (4D0*(F1u/2D0)*h1ure + F1d*h1dre)/9D0
       H1_IM = (4D0*(F1u/2D0)*h1uim + F1d*h1dim)/9D0
           else
+      h1uimag=h1uim
+      h1dimag=h1dim
       H1_RE = (4D0*h1ure + h1dre)/9D0
       H1_IM = (4D0*h1uim + h1dim)/9D0
           endif
+      h1tuimag=gAu*h1tuim
+      h1tdimag=gAd*h1tdim
       H1T_RE = (4D0*gAu*h1ture + gAd*h1tdre)/9D0
       H1T_IM = (4D0*gAu*h1tuim + gAd*h1tdim)/9D0
       E1_RE = (4D0*(F2u/2D0)*e1ure + F2d*e1dre)/9D0
@@ -1352,7 +1549,9 @@ c
       E1T_IM = ha*(4D0*e1tdim + e1tuim)/9D0
         endif
 *
+      return
       end
+
 
       subroutine rtable
 *
@@ -1632,3 +1831,80 @@ C * * * * * * * * * * * * * * * * * * * * * * * * * * *
       IY=IAND(A*IY+C,Z'7FFFFFFF')
       URAND=FLOAT(IY)*S
       END
+
+C********************************************************************* 
+c
+       subroutine vsumm(a,b,c,n)
+       real a(n),b(n),c(n)
+       integer i,n
+        do i=1,n
+          c(i)=a(i)+b(i)
+        enddo
+       return
+       end
+c
+       subroutine vdifm(a,b,c,n)
+       real a(n),b(n),c(n)
+       integer i,n
+        do i=1,n
+          c(i)=a(i)-b(i)
+        enddo
+       return
+       end
+c
+c
+       real function vdotm(a,b,n)
+       real a(n),b(n),s
+       integer i,n
+       s=0.0
+       do i=1,3
+         s=s+a(i)*b(i)
+       enddo
+       if(n.eq.4) s=s-a(n)*b(n)
+       vdotm=s
+       return
+       end
+c   
+       real function vangle(a,b,c,d)
+       real a(3),b(3),c(3),d(3),xm,ym,vcos
+       real x(3),y(3),pi
+       pi=acos(-1.0)
+       call crossm(a,b,x)
+       call crossm(c,d,y)
+       xm=vdotm(x,x,3)
+       ym=vdotm(y,y,3)
+       if(xm.gt.0.0 .and. ym.gt.0.0) then
+         vcos=vdotm(x,y,3)/sqrt(xm)/sqrt(ym)
+         if(abs(vcos).lt.1.0) then
+            vangle=acos(vcos)
+         else
+            if(vcos.ge.1.0)  vangle=0
+            if(vcos.le.-1.0)  vangle=pi
+         endif 
+       else
+         vangle=0
+       endif
+       return
+       end
+c
+       subroutine crossm(a,b,c)
+       real a(3),b(3),c(3)
+       c(1)=a(2)*b(3)-a(3)*b(2)
+       c(2)=a(3)*b(1)-a(1)*b(3)
+       c(3)=a(1)*b(2)-a(2)*b(1)
+       return
+       end
+c
+       real function corraul(delt,dQ2)
+       double precision delt,dQ2
+       real t,Q2,qfac,tfac
+       t=-delt
+       Q2=dQ2
+       if(Q2.lt.1.5) Q2=1.5
+       if(Q2.gt.2.6) Q2=2.6
+       qfac=(0.69*t-1.04*t*t)*(q2-1.5)/1.1+1.0
+       tfac=(0.908-alog(t)/9.5)
+       corraul=1.0/qfac*tfac
+       return 
+       end
+c
